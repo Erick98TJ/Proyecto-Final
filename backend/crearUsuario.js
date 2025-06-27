68681756e03567ef63d71b3753127b89b55be8de
Erick <etenesacaj@unemi.edu.ec>
2025-06-26 22:50:11 -0500

    Agrego cambios en backend y frontend

--- backend/crearUsuario.js
+++ backend/crearUsuario.js
@@ -1,17 +1,17 @@
 const bcrypt = require('bcryptjs');
-const pool = require('./db/connection'); // asegúrate de que esta ruta sea correcta
+const pool = require('./db/connection'); 
 
 const crearUsuario = async () => {
   const nombre = 'Admin';
   const email = 'admin@correo.com';
-  const password = await bcrypt.hash('123456', 10); // cambia si quieres otra clave
+  const password = await bcrypt.hash('123456', 10); 
 
   await pool.query(
     'INSERT INTO usuarios (nombre, email, password) VALUES ($1, $2, $3)',
     [nombre, email, password]
   );
 
-  console.log('✅ Usuario creado');
+  console.log(' Usuario creado');
 };
 
 crearUsuario();
